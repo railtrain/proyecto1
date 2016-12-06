8bd6475f86cc5c02ba7eb8c0de5fc43845ba386a
AdminUTM <adminutm@iMac-de-AdminUTM-3.local>
2016-12-06 09:57:19 -0600

    primer commit

--- AppDemo/ViewController.swift
+++ AppDemo/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let datos = [("alvaro", 20),("erick", 60)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("vista cargada")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return datos.count
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let proto = (indexPath.row % 2 == 0) ? "proto1" : "proto2"
+        
+        let vista = tableView.dequeueReusableCell(withIdentifier: "proto1", for: indexPath) as! filaTableViewCell
+       // vista.lblizq.text = "index"
+       // vista.lblder.text = "\(indexPath.row)"
+        vista.lblizq.text = "\(datos[indexPath.row].0)"
+        vista.lblder.text = "\(datos[indexPath.row].1)"
+        return vista
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("fila\(indexPath.row)")
+    }
 }
 
---
+++ AppDemo/filaTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  filaTableViewCell.swift
+//  AppDemo
+//
+//  Created by AdminUTM on 05/12/16.
+//  Copyright © 2016 AdminUTM. All rights reserved.
+//
+
+import UIKit
+
+class filaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblizq: UILabel!
+    @IBOutlet weak var lblder: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
